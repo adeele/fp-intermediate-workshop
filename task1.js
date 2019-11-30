c2ff73896e44994a207dd63b4ebe878be431dee6
Adrianna Małkiewicz <adrianna.malkiewicz@gmail.com>
2019-11-30 11:05:21 +0100

    Add the first task

---
+++ task1.js
@@ -0,0 +1,20 @@
+/*
+ * TASK #1
+ * Refactor following code in a functional programming way
+ */
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function getOdds(){
+    let odds = [];
+
+    for (let i = 0; i < arr.length + 1; i++) {
+        if (i % 2 !== 0){
+            odds.push(i);
+        }
+    }
+
+    return odds;
+}
+
+console.log(getOdds()); // logs [1, 3, 5, 7, 9]
