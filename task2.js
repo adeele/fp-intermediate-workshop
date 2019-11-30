884a3eb17bbc58098fa5598646309b5ffe4f76ed
Adrianna Małkiewicz <adrianna.malkiewicz@gmail.com>
2019-11-30 15:08:43 +0100

    Add task two

---
+++ task2.js
@@ -0,0 +1,12 @@
+/*
+ * TASK #2
+ * Implement a function that will take groups array and return number of members in all active groups.
+ * Assume that about 100 members in each group are duplicated.
+ */
+
+const groups = [
+    { name: 'JavaScript', isActive: true, members: 700 },
+    { name: 'Angular', isActive: false, members: 500 },
+    { name: 'Node', isActive: true, members: 400 },
+    { name: 'React', isActive: true, members: 600 },
+];
